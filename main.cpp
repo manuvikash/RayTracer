71e251ee3293be775675c83467c8bff854990545
Manuvikash <manuvikashs@gmail.com>
2022-08-03 22:46:35 +0530

    Added a basic red sphere with no shadows

--- main.cpp
+++ main.cpp
@@ -3,7 +3,20 @@
 #include "color.h"
 #include "ray.h"
 
+bool hit_sphere(const point3& center, double radius, const ray& r){
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b*b - 4*a*c; // Quadratic formula
+    return (discriminant > 0);
+    //If the equation has one or more roots, the ray collides with the sphere
+}
+
+
 color ray_color(const ray& r){
+    if(hit_sphere(point3(0,0,-1), 0.5, r))
+        return color(1,0,0);
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0-t) * color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
